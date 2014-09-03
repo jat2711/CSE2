ec4c3e5d08c78d417313c2bab80a94a1b1a2cbe0
jat2711 <jot318@lehigh.edu>
2014-09-03 01:27:11 +0000

    updated  WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+////////////////////
+//Joanna Targowski
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statement with box around it
+    System.out.println("             -----------                   ");
+    System.out.println("             | Welcome |                   ");
+    System.out.println("             -----------                   ");
+    System.out.println("       ^    ^    ^    ^    ^    ^          ");
+    System.out.println("      / \\  / \\  / \\  / \\  / \\  / \\   ");
+    System.out.println("           |-J--O--T--3--1--8-|            ");
+    System.out.println("      \\ /  \\ /  \\ /  \\ /  \\ /  \\ /   ");
+    System.out.println("       v    v    v    v    v    v          ");
+    System.out.println("    I love fried green tea ice cream.      ");
+    System.out.println("test");
+    
+    //test/testtest
+ }
+}
