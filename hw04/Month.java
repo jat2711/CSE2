224771461566bf5a967ed2fc56906a1c53d35451
jat2711 <jot318@lehigh.edu>
2014-09-24 02:08:33 +0000

    Done with Month

--- hw04/Month.java
+++ hw04/Month.java
@@ -15,45 +15,61 @@ import java.util.Scanner;
 public class Month{
     
 //  add main method 
-        public static void main(String[]) { 
+        public static void main(String[] args) { 
             
         Scanner myScanner;
-        myScanner=new Scanner (System.in)
+        myScanner=new Scanner (System.in);
         
         System.out.println("Enter an int giving the number of the month (1-12) ");   // Check if the integer is entered for the month and it is in the required range.
+        
+        if (myScanner.hasNextInt()) {
         int nMonth=myScanner.nextInt();
-        if (myScanner.hasNextInt())
+        if (nMonth <1 || nMonth >12) {
+                System.out.println("The value entered is not in the range");
+                return; // This terminates the program.
+        } 
+        switch (nMonth) {
+                case 1: System.out.println("January has 31 days.");
+                        break;
+                case 2: System.out.println("Please enter the year");
+                        int Year=myScanner.nextInt(); // Declaring the year.
+                        if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)){
+                                System.out.println("In this year February has 29 days.");
+                        }               
+                        else {
+                                System.out.println("In this year February has 28 days."); 
+                        }
+                        break;
+                case 3: System.out.println("March has 31 days.");
+                        break;
+                case 4: System.out.println("April has 30 days.");
+                        break;
+                case 5: System.out.println("May has 31 days.");
+                        break;
+                case 6: System.out.println("June has 30 days.");
+                        break;
+                case 7: System.out.println("July has 31 days.");
+                        break;
+                case 8: System.out.println("August has 31 days.");
+                        break;
+                case 9: System.out.println("September has 30 days.");
+                        break;
+                case 10: System.out.println("October has 31 days.");
+                        break;
+                case 11: System.out.println("November has 30 days.");
+                        break;
+                case 12: System.out.println("December has 31 days.");
+                        break;
+        
+        } // end of the first switch statement
+        } //end of the first if statement
+
+        
         
         else {
-                System.out.println("Are you sure the amount entered is an integer and is in the range specified?");
+                System.out.println("You did not enter an int");
+                return; // This terminates the program. 
         }
-        switch (Month) {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                case 10:
-                case 11:
-                case 12:
-        
-        if (MonthNumber==2) {
-                System.out.println("Please enter the year");
-                if ((Year % 4 == 0) && (Year % 400 == 0) && ! (Year % 100 == 0))
-        }
-        
-        if month
-                
-        }
-        
-        
-        
-        
-        
-
+       
 } // end of main method 
 } // end of class
