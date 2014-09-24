62a8b2f788d8ae1ec6ef236d17840bf3e5de6d01
jat2711 <jot318@lehigh.edu>
2014-09-24 22:26:48 +0000

    finished CourseNumber

--- hw04/CourseNumber.java
+++ hw04/CourseNumber.java
@@ -21,9 +21,43 @@ public class CourseNumber{
         
         System.out.println("Enter a six digit number giving the course semester."); // Asks the user to enter a six digit number.
         int digit=myScanner.nextInt();
-        // Start calculations
         
+        if (digit>186510 && digit<201440) {
+                
+                int digit1=(digit%10);
+                int digit2=(digit/10)%10;
+                int digit3=(digit/100)%10;
+                int digit4=(digit/1000)%10;
+                int digit5=(digit/10000)%10;
+                int digit6=(digit/100000)%10;
+                
+        
+        if (digit1==0) {
+                switch(digit2) {
+                        case 1: System.out.println("The course was offered in Spring semester of " +digit6+ +digit5+ +digit4+ +digit3);
+                                break;
+                        case 2: System.out.println("The course was offered in the Summer 1 semester of " +digit6+ +digit5+ +digit4+ +digit3);
+                                break;
+                        case 3: System.out.println("The course was offered in the Summer 2 semester of " +digit6+ +digit5+ +digit4+ +digit3);
+                                break;
+                        case 4: System.out.println("The course was offered in the Fall semester of " +digit6+ +digit5+ +digit4+ +digit3);
+                                break;
+                                
+                } // end of switch statement
+        }
+        else {
+                System.out.println(""+digit1 + digit2 + " is not a legitimate semester.");
+                return; // This terminates the program because the number does not repsond to a semester.
+        } // end of else statement
+        
+        } // end of first if statement
+       else {
+               System.out.println("The number you entered was outside the range.");
+                return; // This terminates the program. 
+       } // end of else statement
+   
         
 
-} // end of main method 
+
+} // end of main method
 } // end of class
